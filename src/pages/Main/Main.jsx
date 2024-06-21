cc24113d52c107ae0336a63aefca43622935ef59
Daniil <forfaceitsemik@gmail.com>
2024-06-21 18:12:08 +0300

    added icons and tg support

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,16 +1,27 @@
 import './Main.css'
+import settingsIcon from '../../assets/icons/settings_icon.png'
+import walletIcon from '../../assets/icons/wallet_icon.png'
+
 export default function Main({data, handleCoinClick}) {
+    const tg = window.Telegram.WebApp
+    const avatar = tg.initDataUnsafe.user ? tg.initDataUnsafe.user.photo_url : ''
     return (
         <div className="main">
             <div className="main_header">
-                <div className="main_header_avatar"></div>
-                <div className="main_header_popup push"></div>
-                <div className="main_header_popup"></div>
-                <div className="main_header_popup"></div>
+                <div className="main_header_avatar" style={{backgroundImage: `url('${avatar}')`}}></div>
+                <div className="main_header_popup push">
+                    <div className="main_header_icon"  style={{backgroundImage: `url('${settingsIcon}'`}}></div>
+                </div>
+                <div className="main_header_popup">
+                    <div className="main_header_icon"  style={{backgroundImage: `url('${walletIcon}'`}}></div>
+                </div>
+                <div className="main_header_popup">
+                    <div className="main_header_icon">?</div>
+                </div>
             </div>
             <div className="coins">
                 <div className="coins_icon"></div>
-                <p className="coins_count">{renderCoins(data.coins)}</p>
+                <p className="coins_count">{data.coins}</p>
             </div>
             <div className="coin" onClick={handleCoinClick}>
                 <div className="coin_image"></div>
@@ -25,19 +36,19 @@ export default function Main({data, handleCoinClick}) {
 
 
 
-function renderCoins(coins) {
-    // let strCoins = `${coins}`
-    // if(coins < 100000) {
-    //     return strCoins
-    // }
-    // if(coins >= 1000000000) {
-    //   return strCoins.slice(-12,-9) + 'B'
-    // }
-    // if(coins >= 1000000) {
-    //   return strCoins.slice(-9,-6) + 'M'
-    // }
-    // if(coins >= 100000) {
-    //   return strCoins.slice(-6,-3) + 'K'
-    // }
-    return coins
-}
+// function renderCoins(coins) {
+//     // let strCoins = `${coins}`
+//     // if(coins < 100000) {
+//     //     return strCoins
+//     // }
+//     // if(coins >= 1000000000) {
+//     //   return strCoins.slice(-12,-9) + 'B'
+//     // }
+//     // if(coins >= 1000000) {
+//     //   return strCoins.slice(-9,-6) + 'M'
+//     // }
+//     // if(coins >= 100000) {
+//     //   return strCoins.slice(-6,-3) + 'K'
+//     // }
+//     return coins
+// }
